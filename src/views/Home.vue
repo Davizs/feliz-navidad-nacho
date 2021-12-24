6c2c618a15c2c7060c87d1419205072699a44081
David Gallego Gómez <davidgallegogomez@yahoo.es>
2021-12-24 13:02:48 +0100

    ENH: Test to upload to git-pages

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="STEAM te desea FELIZ NAVIDAD!!!" />
   </div>
 </template>
 
